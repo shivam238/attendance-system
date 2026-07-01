586ac18c16efb8c4824e5ba6eaa29f81d7eb1f29
Shivam Kumar Mahto <sm3165599@gmail.com>
2026-07-01 20:58:13 +0530

    app fixing try 2

--- android/app/src/main/java/com/attendify/app/MainActivity.java
+++ android/app/src/main/java/com/attendify/app/MainActivity.java
@@ -2,12 +2,19 @@ package com.attendify.app;
 
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
+import com.google.firebase.FirebaseApp;
 
 public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(MockLocationChecker.class);
+        try {
+            FirebaseApp.initializeApp(this);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         super.onCreate(savedInstanceState);
     }
 }
+
 
