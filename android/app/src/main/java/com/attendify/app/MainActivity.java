eeb97c5cb020abf26b5a661ddf85843fc4a89e04
Shivam Kumar Mahto <sm3165599@gmail.com>
2026-07-01 19:56:06 +0530

    fix: register MockLocationChecker plugin and sync app

--- android/app/src/main/java/com/attendify/app/MainActivity.java
+++ android/app/src/main/java/com/attendify/app/MainActivity.java
@@ -1,5 +1,13 @@
 package com.attendify.app;
 
+import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        registerPlugin(MockLocationChecker.class);
+        super.onCreate(savedInstanceState);
+    }
+}
+
