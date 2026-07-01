6efc3f8880d07acb277d98bce4d026302e957b95
Shivam Kumar Mahto <sm3165599@gmail.com>
2026-07-01 22:48:14 +0530

    chore: rollback to 448eafb (fix: restore missing About and Feedback links in app footer)

--- android/app/src/main/java/com/attendify/app/MainActivity.java
+++ android/app/src/main/java/com/attendify/app/MainActivity.java
@@ -1,13 +1,5 @@
 package com.attendify.app;
 
-import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        registerPlugin(MockLocationChecker.class);
-        super.onCreate(savedInstanceState);
-    }
-}
-
+public class MainActivity extends BridgeActivity {}
